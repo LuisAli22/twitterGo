6407550a01bc2713ded7e21d1b2bbd23c6a9faa5
LuisAli22 <LuisAli22@users.noreply.github.com>
2024-04-18 12:43:09 -0300

    Corrección espacios en string json

--- models/sm.go
+++ models/sm.go
@@ -8,9 +8,9 @@ porque necesitamos que sean publicos y usarlos
 en otro paquete
 */
 type Secret struct {
-	Host     string `json: "host"`
-	Username string `json: "username"`
-	Password string `json: "password"`
-	JWTSign  string `json: "jwtsign"`
-	Database string `json: "database"`
+	Host     string `json:"host"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	JWTSign  string `json:"jwtsign"`
+	Database string `json:"database"`
 }
